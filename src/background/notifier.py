1eaeba9526c2ea5964d7050b5e26202fdb3e8486
JibrilExe <cedric00ik@gmail.com>
2025-07-29 09:08:38 +0200

    code cleanup

--- src/background/notifier.py
+++ src/background/notifier.py
@@ -1,14 +1,15 @@
+"""
+This background worker will query the active events table every few seconds
+and send notifications for the events that are within 5 minutes, if this
+is the case, they are also moved to the past database
+"""
 from time import sleep
 from ..services.eventdb import get_active, active_to_past
 from datetime import datetime, timedelta, timezone
 
-# This background worker will query the active events table every 10 seconds
-# and send notifications for the events that are within 5 minutes, if this
-# is the case, they are also moved to the past database
+
 
 if __name__ == "__main__":
-    subscribers = ["Jef", "Katy", "Borogrove"] # idea is that we can have multiple instances of notifiers workers
-    # with each their list of subscribers
     delta_fivemin = timedelta(minutes=5)
     while True:
         sleep(5)
